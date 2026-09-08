937535a1420bb3f0be95d0bbfe1adc7e298a0ec1
AndreyKhoroshev <andrey.khoroshev@myy.haaga-helia.fi>
2026-09-08 08:45:34 +0300

    Added user & event repo

---
+++ calendar-backend/src/main/java/hh/syntax/calendar/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package hh.syntax.calendar.repository;
+
+import hh.syntax.calendar.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
